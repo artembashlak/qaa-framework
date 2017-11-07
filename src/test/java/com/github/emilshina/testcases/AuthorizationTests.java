dca5cbed75b3fdfb82585b5fccda9cff6ecfaeb7
Elena Milshina <wavemilshina@alarm.com>
2017-11-07 23:02:44 +0200

    Data supplier, listeners, assertions: - added quality plugin; - created data suppliers; - added listeners; - created custom annotation; - added assertions; - used lombok plugin; - replaced classic logger with @Slf4j; - clean up.

--- src/test/java/com/github/emilshina/testcases/AuthorizationTests.java
+++ src/test/java/com/github/emilshina/testcases/AuthorizationTests.java
@@ -1,33 +1,57 @@
 package com.github.emilshina.testcases;
 
+import com.github.emilshina.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.SoftAssertions;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+/**
+ * Authorization tests.
+ */
+@Slf4j
 public class AuthorizationTests extends BaseClass {
 
     @BeforeMethod
-    public void beforeEachMethod() {
-        System.out.println("Before method of Auth class.");
+    public void beforeEachMethod(final ITestContext context, final Method method) {
+        final String browser = context.getCurrentXmlTest().getParameter("browser");
+        if (log.isInfoEnabled()) {
+            log.info("Before method of Auth class in browser " + browser + method.getName());
+        }
     }
 
     @AfterMethod
     public void afterEachMethod() {
-        System.out.println("After method of Auth class.");
+        log.info("After method of Auth class.");
     }
 
-    @Test
-    public void userShouldBeAuthorizedWithValidCredentials() {
-        System.out.println("User was authorized successfully.");
+    @Test(dataProvider = "userCredentials", dataProviderClass = DataSuppliers.class)
+    public void userShouldBeAuthorizedWithValidCredentials(final HashMap.SimpleEntry credentials) {
+        if (log.isInfoEnabled()) {
+            log.info("com.github.emilshina.model.User was authorized successfully. With " + credentials);
+        }
     }
 
-    @Test
-    public void userShouldNotBeAuthorizedWithInvalidLogin() {
-        System.out.println("Invalid credentials.");
+    @Data(source = "data.json")
+    @Test(dataProvider = "usersWithInvalidCredentials", dataProviderClass = DataSuppliers.class)
+    public void userShouldNotBeAuthorizedWithInvalidLogin(final User user) {
+        final SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(user.getUserName()).as("Username verification.").isEqualTo("username1");
+        if (log.isInfoEnabled()) {
+            log.info("Invalid credentials for: " + user.getUserName() + ".");
+        }
+        softAssertions.assertAll();
     }
 
-    @Test
-    public void userShouldNotBeAuthorizedWithInvalidPassword() {
-        System.out.println("Invalid credentials. Try again.");
+    @Test(dataProvider = "invalidPassword", dataProviderClass = DataSuppliers.class)
+    public void userShouldNotBeAuthorizedWithInvalidPassword(final String password) {
+        if (log.isInfoEnabled()) {
+            log.info("Invalid credentials. Try again. " + password);
+        }
     }
 }
